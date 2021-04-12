953a8469e15b591553069dc7570afd47bc0f0f7a
jojo-tey <dosagam6301@gmail.com>
2021-04-13 01:37:12 +0200

    tester_ec2

---
+++ ec2_tester/variable.tf
@@ -0,0 +1,17 @@
+variable "AWS_ACCESS_KEY" {
+  type = string
+}
+
+variable "AWS_SECRET_KEY" {
+  type = string
+}
+
+variable "AWS_REGION" {
+  type = string
+}
+
+variable "AMIS" {
+  type = string
+}
+
+
